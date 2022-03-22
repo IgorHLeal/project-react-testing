1c26112d1f4e96449affd88fc6860afaa1d44ca3
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-22 18:53:19 -0300

    test: Implementação dos testes para FavoritePokémons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+it('Verifica se é exibido na tela a mensagem No favorite pokemon found', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  const paragraph = screen.getByText('No favorite pokemon found');
+  expect(paragraph).toBeDefined();
+});
+
+it('se é exibido todos os cards de pokémons favoritados', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  const favorite = screen.getByRole('heading',
+    { name: /Favorite Pokémons/i, level: 2 });
+
+  expect(favorite).toBeDefined();
+});
