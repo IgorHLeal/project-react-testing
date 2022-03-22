1567b8dac0a91e327aec71213c5bb530f77cb899
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-22 17:33:31 -0300

    test: implementação dos testes para About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
 
 it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
   render(
@@ -16,14 +17,34 @@ it('Teste se a página contém um heading h2 com o texto About Pokédex', () => 
 });
 
 it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-  render(
-    <MemoryRouter>
-      <About />
-    </MemoryRouter>,
+  renderWithRouter(<About />);
+
+  // Foi preciso usar template literal por causa do tamanho máximo da linha
+  const firstParagraphOne = 'This application simulates a Pokédex';
+  const firstParagraphTwo = 'a digital encyclopedia containing all Pokémons';
+  const firstParagraph = `${firstParagraphOne} ${firstParagraphTwo}`;
+
+  // Precisei colocar os parênteses por causa do tamanho máximo da linha
+  const scnd = (
+    'One can filter Pokémons by type, and see more details for each one of them'
   );
-  const firstParagraph = screen.getByRole('This application simulates a Pokédex');
-  const secondParagraph = screen.getByText('One can filter Pokémons by typ');
 
   expect(firstParagraph).toBeDefined();
-  expect(secondParagraph).toBeDefined();
+  expect(scnd).toBeDefined();
+
+  // ---------- Segunda opção - Entender onde está falhando ----------
+
+  /* const { sectionParagraphs } = renderWithRouter(<About />);
+  const paragraphs = sectionParagraphs.querySelector('p');
+
+  expect(paragraphs).toHaveLength(2); */
+});
+
+it('Teste se a página contém a imagem de uma Pokédex', () => {
+  renderWithRouter(<About />);
+
+  const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  const image = screen.getByRole('img', { name: /Pokédex/i });
+
+  expect(image.src).toBe(url);
 });
